ed4cb1785bb9845c21f538d3dff1958eb67d21f1
Johanne Pinel <johanne.pinel@epfl.ch>
2026-04-20 14:20:48 +0200

    test vision très lent

--- miniproject/submission/controller.py
+++ miniproject/submission/controller.py
@@ -12,6 +12,12 @@ class Controller:
         #johanne code
         self.odor_smooth = None
         self.alpha = 0.1  # smoothing factor for the low-pass filter
+                
+                
+        self.vision_frames = []  # frames lisibles pour affichage
+
+
+
 
     def step(self, sim: MiniprojectSimulation):
         # implement your control algorithm here
@@ -19,11 +25,9 @@ class Controller:
 
         #print(f"Olfaction: {olfaction}")  # for debugging purposes
         # get other observations as needed
-        drives = np.array([2.0, 2.0])  # replace with your control logic
+        # drives = np.array([2.0, 2.0])  # replace with your control logic
         
         ################################### test joh ##################################
-
-
 
         if self.odor_smooth is None:
             self.odor_smooth = olfaction
@@ -31,22 +35,40 @@ class Controller:
             self.odor_smooth = (1 - self.alpha) * self.odor_smooth + self.alpha * olfaction
 
 
-        drives = _odor_to_drives(self.odor_smooth)
+        drives = odor_to_drives(self.odor_smooth)
                 
 
-        #drives = _odor_to_drives(self.odor_smooth)
+        ommatidia = sim.get_ommatidia_readouts(sim.fly.name)
+        im = np.concatenate([
+            sim.fly.retina.hex_pxls_to_human_readable(eye.max(-1), color_8bit=True)
+            for eye in ommatidia
+        ], axis=1)
+        self.vision_frames.append(im)
+        left_intensity = ommatidia[0].mean()
+        right_intensity = ommatidia[1].mean()
+
+        obstacle_left = 1 - left_intensity 
+        obstacle_right = 1 - right_intensity
+
+        obstacle_threshold = 0.5
+        avoidance_strength = 1.5  # à ajuster
+
+        if obstacle_left > obstacle_threshold or obstacle_right > obstacle_threshold:
+            if obstacle_left > obstacle_right:
+                drives[1] += avoidance_strength
+            else:
+                drives[0] += avoidance_strength
+
         ###############################################################################
 
         joint_angles, adhesion = self.turning_controller.step(drives)
         return joint_angles, adhesion
 
-
-def _odor_to_drives(odor_intensities, attractive_gain=-500, aversive_gain=80):
-    # odor_intensities shape : (4, n_sources)
-    # n_sources peut être 1 (attractive seulement) ou 2 (attractive + aversive)
+#inspiré de la fontion du lab 4
+def odor_to_drives(odor_intensities, attractive_gain=-500, aversive_gain=80): 
     n_sources = odor_intensities.shape[1]
 
-    # Source attractive (dimension 0 — toujours présente)
+    # Source attractive 
     attractive = np.average(
         odor_intensities[:, 0].reshape(2, 2), axis=0, weights=[9, 1]
     )
