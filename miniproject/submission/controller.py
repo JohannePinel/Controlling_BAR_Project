334f7a0db8fea6dda2cd0b3ef78af693aa710b81
Keinoma <adiren.cheretakis@epfl.ch>
2026-04-20 22:44:27 +0200

    detecte horizon

--- miniproject/submission/controller.py
+++ miniproject/submission/controller.py
@@ -16,3 +16,32 @@ class Controller:
         drives = np.array([1.0, 1.0])  # replace with your control logic
         joint_angles, adhesion = self.turning_controller.step(drives)
         return joint_angles, adhesion
+
+###########################################################
+#################### TEST ADRI ############################
+###########################################################
+    can_walk = False
+
+    def stops_analysing(self, sim: MiniprojectSimulation):
+        self.can_walk = True
+        return 0
+    
+    def starts_analysing(self, sim: MiniprojectSimulation):
+        self.can_walk = False
+        return 0
+    
+    def looks_for_bananas(self, sim: MiniprojectSimulation):
+        self.can_walk = False
+
+        return 0
+    
+    def detects_horizon_while_stopped(self, sim: MiniprojectSimulation, screenshot):
+        self.can_walk = False
+
+        height_of_horizon = 0
+        for i in range(5):
+            if screenshot[261+i, 100][1] == 128:
+                height_of_horizon = 261+i
+                break
+        self.can_walk = True
+        return height_of_horizon
