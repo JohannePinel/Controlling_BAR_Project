dfaa08b0cd447cb192096a7b7c0783c169ca9989
Keinoma <adiren.cheretakis@epfl.ch>
2026-04-21 15:54:10 +0200

    detect obstacle non debug

--- miniproject/submission/controller.py
+++ miniproject/submission/controller.py
@@ -8,12 +8,33 @@ class Controller:
         from flygym.examples.locomotion import TurningController
 
         self.turning_controller = TurningController(sim.timestep)
+        
+        self.frames = [] # must be out of step()
+        self.count = 0
+        self.height_obstacles = 400 # manually tuned
+        self.starts_scanning = 200 # manually tuned
+        self.starts_comparing = 360 # manually tuned
+        self.different_objects = 20 # manually tuned  
+        self.color_of_grass = 90 # manually tuned
 
     def step(self, sim: MiniprojectSimulation):
+        self.count += 1
         # implement your control algorithm here
         olfaction = sim.get_olfaction(sim.fly.name)
+
+        # color vision
+        if self.count % 100 == 0:
+            self.im = np.concatenate(
+                [
+                    sim.get_raw_vision(sim.fly.name)[0],
+                    sim.get_raw_vision(sim.fly.name)[1]
+                    
+                ], axis=1
+            )
+            self.frames.append(self.im)
+
         # get other observations as needed
-        drives = np.array([1.0, 1.0])  # replace with your control logic
+        drives = np.array([0.2, 0.2])  # replace with your control logic
         joint_angles, adhesion = self.turning_controller.step(drives)
         return joint_angles, adhesion
 
@@ -45,3 +66,34 @@ class Controller:
                 break
         self.can_walk = True
         return height_of_horizon
+    
+    def checks_for_ostacles(self, sim: MiniprojectSimulation, ):
+        intensities = [] # length = nb of pixels checked ; (r, g, b) for each pixel
+        obstacle_pos = 0 # on the hizontal axis at self.height_obstacles 
+
+
+        for i in range(900-(2*self.starts_scanning)): # frames are 900 wide
+            pixel = self.frames[-1][self.height_obstacles,i]
+
+            if self.worth_analysing(sim, pixel, i)[1]: # make sure we are not in the black spot
+                intensities.append(self.frames[-1][self.height_obstacles, self.starts_scanning+i])
+            
+            if self.worth_analysing(sim, pixel, i)[0]: # enough datas to have a good mean to compare the pixel to
+                if np.abs(intensities[-1] - np.mean(intensities[:-1])) > self.different_objects : 
+                    obstacle_pos = (self.starts_scanning + i)
+                    break
+
+
+        return obstacle_pos
+    
+
+    def worth_analysing(self, sim: MiniprojectSimulation, pixel, horizontal_pos):
+        result = [False, False]
+        
+        if (horizontal_pos + self.starts_scanning) > self.starts_comparing :
+            result[0] = True # already enough data to have a good mean to compare the pixel to
+
+        if pixel[1]>self.color_of_grass:  
+            result[1] = True # not on the black spot
+         
+        return result
