0d93aa725351724c4ae87e9aa142695db1a357c0
unknown <estelle.baumann@epfl.ch>
2026-05-22 19:34:04 +0200

    success

--- miniproject/submission/controller.py
+++ miniproject/submission/controller.py
@@ -241,8 +241,6 @@ class Controller:
                 self.odor_state = "LOST"
                 # even with forward wind we do not smell it, it is really lost, need to search again
 
-        if self.count == 39000 : 
-            print('mean_odor is : ' ,mean_odor)
         if mean_odor > 1.4e-5 :
             self.odor_state = "LUNCH TIME"
 
@@ -406,20 +404,20 @@ class Controller:
         
         if self.current_slope_category == "carreful_upsidedown": 
             self.speed = self.speed * 0.8  # walk slighlty slower in slopes
-            #print("we stuck")
+
            
         if self.wind_state == "SIDE":
             self.speed = self.speed* 0.7 # walk slighlty slower when there is wind on the side 
-            #print('we stuuuuuuuuuck')
+
 
         if self.hilltop_timer > 0 and self.general_state not in ("RUNNING", "FLIPPED"): # hilltop: brief caution when pitch transitions ascending → flat/descending
             self.speed = self.speed * 0.7
             self.hilltop_timer -= 1
-            #print('aaaaaahhhhhhhhhh')
+
         
         if self.current_slope_category == "carreful_upsidedown" and self.wind_state == "SIDE" and self.general_state != "RUNNING":  
             self.speed = self.speed* 0.2 # very likely to fall, we slow down a lot
-            #print(' WE VERY VERY STUCK')
+
 
         if self.odor_state == "LUNCH TIME" :
             self.speed = 0
